2ce519a07f5eb03d5b51af45d8690760fe71dced
alex0112 <alarsen0112@gmail.com>
2019-06-27 22:39:24 -0600

    Initial tests for 'new' function.  Porting from infocoin project.

--- test/aurum_test.exs
+++ test/aurum_test.exs
@@ -2,7 +2,22 @@ defmodule AurumTest do
   use ExUnit.Case
   doctest Aurum
 
-  test "new" do
-    
+  setup_all do
+    ##[ client: Client.new(ENV['COINBASE_KEY'], ENV['COINBASE_SECRET']) ] ## TODO: Implement
+    [ client: nil ]
+  end
+  
+  describe "new" do
+    test "raises an error without symbol", context do
+      assert_raise ArgumentError, Aurum.new(context[:client])
+    end
+
+    test "raises an error without a client" do
+      assert_raise ArgumentError, Aurum.new(:BTC)
+    end
+
+    test "does not raise an error when provided with a client and symbol", context do
+      assert Aurum.new(context[:client], :BTC)
+    end
   end
 end
