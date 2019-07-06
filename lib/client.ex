6c952f7f78793a7ecd6e2cb4fda62c3959f81164
alex0112 <alarsen0112@gmail.com>
2019-07-05 19:16:26 -0600

    Moved server_time call to request.ex

--- lib/client.ex
+++ lib/client.ex
@@ -9,11 +9,5 @@ defmodule Client do
   def new(api_key, api_secret) do
     %Client{api_key: api_key, api_secret: api_secret}
   end
-
-  def server_time do
-    response = Poison.decode! HTTPotion.get("https://api.coinbase.com/v2/time").body
-    response["data"]["epoch"]
-  end
   
 end
-
--- lib/request.ex
+++ lib/request.ex
@@ -1,16 +1,27 @@
 defmodule Request do
-  defstruct method: "", path: "", body: ""
+  defstruct method: "", path: "", body: "", agent: nil
 
   require HTTPotion
+  require Poison
   
   def new(method, path, body) do
     if !(Enum.member? ["GET", "POST", "PUT", "PATCH", "DELETE"], method), do: raise ArgumentError
-    %Request{method: method, path: path, body: body}
+    
+    request = %Request{method: method, path: path, body: body}
+    signed_request = Request.sign!(request, 'TODO: get secret from env', Request.server_time)
+    
   end
+
+  defp server_time do
+    response = Poison.decode! HTTPotion.get("https://api.coinbase.com/v2/time").body
+    response["data"]["epoch"]
+  end
+
   
-  defp sign(request, secret, server_time) do
-    pre_hash = Client.server_time <> request.method <> request.path <> request.body
+  defp sign!(request, secret, server_time) do
+    pre_hash = server_time <> request.method <> request.path <> request.body
     signature = :crypto.hmac(:sha256, secret, pre_hash) |> Base.encode16 |> String.downcase
   end
   
 end
+
