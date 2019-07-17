6fdaaf64668308447566a8a6bb23e4ab9bc261cb
alex0112 <alarsen0112@gmail.com>
2019-07-16 18:28:51 -0600

    Closes #5: sha256 hmac is now base64 encoded, and other minor tweaks.

--- lib/request.ex
+++ lib/request.ex
@@ -6,19 +6,30 @@ defmodule Request do
   
   def new(method, path, body, key, secret, server_time) do
     if !(Enum.member? [:GET, :POST, :PUT, :PATCH, :DELETE], method), do: raise ArgumentError, message: "Unrecognized HTTP method #{method}"
-    request = %Request{ method: method, path: path, body: body, key: key, secret: secret, timestamp: server_time, signature: nil }
-    Request.sign!(request, secret)
+
+    base_url      = "api.coinbase.com/v2"
+    full_path     = base_url <> path
+    request       = %Request{
+        method: method,
+        path: full_path,
+        body: body,
+        key: key,
+        secret: secret,
+        timestamp: server_time,
+        signature: nil,
+    }
+
+    Request.sign!(request)
   end
 
-  def sign!(request, secret) do
+  def sign!(request) do
     pre_hash = Integer.to_string(request.timestamp) <> Atom.to_string(request.method) <> request.path <> request.body
-    IO.puts pre_hash
-    signature = :crypto.hmac(:sha256, secret, pre_hash) |> Base.encode16 |> String.downcase
-    %Request{ method: request.method, path: request.path, timestamp: request.timestamp, signature: signature }
+    signature = :crypto.hmac(:sha256, request.secret, pre_hash) |> Base.encode64 #|> String.downcase
+    %Request{ method: request.method, path: request.path, key: request.key, secret: request.secret, timestamp: request.timestamp, signature: signature }
   end
   
   def send!(request) do
-    base_url = "https://api.coinbase.com/"
+    tls_base = "https://"
     payload = [
       body: request.body,
       headers:
@@ -29,18 +40,17 @@ defmodule Request do
         "Content-Type": "application/json",
       ]
     ]
-    IO.puts base_url <> request.path
     case request.method do
       :GET ->
-        HTTPotion.get base_url <> request.path, payload
+        HTTPotion.get request.path, payload
       :POST ->
-        HTTPotion.post base_url <> request.path, payload
+        HTTPotion.post request.path, payload
       :PUT ->
-        HTTPotion.put base_url <> request.path, payload
+        HTTPotion.put request.path, payload
       :PATCH ->
-        HTTPotion.patch base_url <> request.path, payload
+        HTTPotion.patch request.path, payload
       :DELETE ->
-        HTTPotion.delete base_url <> request.path, payload
+        HTTPotion.delete request.path, payload
       _ ->
         raise "Unrecognized HTTP verb '#{request.method}'"
     end
